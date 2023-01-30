37fc51399e260c86a1f950173296094db19acd62
adityasshinde <s.adityashinde11@gmail.com>
2023-01-30 23:40:51 +0530

    SuperKey issue resolved

--- mainBackend/backend/settings.py
+++ mainBackend/backend/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9ukuarvi0_t_$2a&numpx5t*&t*ir+%_!59=aro^-d=btajf6v'
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
